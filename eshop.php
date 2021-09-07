343ccbf0b494403f6c423f1b208a5c133352d8b7
Ncesecrede <francescopm95@gmail.com>
2021-09-07 14:08:21 +0100

    Fir

---
+++ eshop.php
@@ -0,0 +1,28 @@
+<?php 
+    class Eshop{
+
+        public $name;
+
+        public $address;
+    }
+
+    //--------------------
+
+    class Product{
+
+        public $price;
+
+    }
+
+    class SportProduct extends Product{
+
+    }
+
+    class HouseProduct extends Product{
+        
+    }
+
+    class Teachproduct extends Product{
+        
+    }
+?>
